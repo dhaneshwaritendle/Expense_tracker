85be2b73c9d45b5c8c67018ddd168beeb9ed2f12
Dhaneshwari Tendle <110600266+dhaneshwaritendle@users.noreply.github.com>
2026-07-07 19:16:47 +0530

    Update CORS configuration to comment out localhost
    Commented out the localhost origin in the CORS configuration.

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 const allowedOrigins = [
-  'http://localhost:5173',
+  // 'http://localhost:5173',
   'https://expense-tracker-1-mpy1.onrender.com' 
 ];
 app.use(cors({
